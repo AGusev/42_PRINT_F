d46a223e15835eb01fdb0d5b01997d2308458874
Aleksandr Gusev <agusev@e1z4r14p9.42.us.org>
2019-03-22 00:08:49 -0700

    21

--- ft_printf.c
+++ ft_printf.c
@@ -6,34 +6,42 @@
 /*   By: agusev <agusev@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/13 17:59:02 by agusev            #+#    #+#             */
-/*   Updated: 2019/03/17 22:46:03 by agusev           ###   ########.fr       */
+/*   Updated: 2019/03/22 00:08:35 by agusev           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-int				narrow_ft_printf(const char *format, va_list *arg, char *str)
+int			final_print(const char *format, char *str)
 {
-	t_flags				flags;
-	char				*next;
-	char				*tmp;
-	static int			len;
+	int		length;
 
-	if (!len)
-		len = 0;
+	str = ft_update(str, ft_strjoin(str, format));
+	write(1, str, ft_strlen(str));
+	length = ft_strlen(str);
+	free(str);
+	return (length);
+}
+
+int			narrow_ft_printf(const char *format, va_list *arg, char *str)
+{
+	t_flags		flags;
+	char		*next;
+	char		*tmp;
+	static int	length;
+
+	if (!length)
+		length = 0;
 	next = ft_strchr(format, '%');
 	if (next == NULL)
-		return (len + final_print(format, str));
+		return (length + final_print(format, str));
 	tmp = ft_strccrt(format, '%');
 	str = ft_update(str, ft_strjoin(str, tmp));
 	free(tmp);
 	++next;
 	ft_bzero(&flags, sizeof(t_flags));
 	parse_flags(&next, &flags, arg);
-	tmp = conversions(&flags, arg, &len, &str);
+	tmp = conversions(&flags, arg, &length, &str);
 	str = ft_update(str, ft_strjoin(str, tmp));
 	++next;
 	free(tmp);
@@ -41,27 +49,16 @@ int				narrow_ft_printf(const char *format, va_list *arg, char *str)
 	return (narrow_ft_printf(next, arg, str));
 }
 
-int				final_print(const char *format, char *str)
+int			ft_printf(const char *format, ...)
 {
-	int		len;
-
-	str = ft_update(str, ft_strjoin(str, format));
-	write(1, str, ft_strlen(str));
-	len = ft_strlen(str);
-	free(str);
-	return (len);
-}
-
-int				ft_printf(const char *format, ...)
-{
-	int			len;
+	int			length;
 	char		*str;
 	va_list		arg;
 
-	len = 0;
+	length = 0;
 	str = ft_strnew(1);
 	va_start(arg, format);
-	len = narrow_ft_printf(format, &arg, str);
+	length = narrow_ft_printf(format, &arg, str);
 	va_end(arg);
-	return (len);
+	return (length);
 }
